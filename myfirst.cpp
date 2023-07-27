da2c291d7c75c5b9df5b1892d7c99ce4541d7ffe
HuiqiXia-China <huiqixia@icloud.com>
2023-07-27 15:05:20 +0800

    update_test

---
+++ myfirst.cpp
@@ -0,0 +1,10 @@
+//display a message
+#include<iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time";
+    cout << endl;
+    cout << "you won't regret it!" << endl;
+    return 0;
+}
